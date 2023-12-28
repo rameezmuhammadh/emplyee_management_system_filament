a9dfd55618cd80344bf6d3a82875265c021460bd
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-28 23:10:10 +0530

    feat: fetch the data to table, created infolist (quick view)

--- app/Filament/Resources/StateResource.php
+++ app/Filament/Resources/StateResource.php
@@ -2,16 +2,27 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\StateResource\Pages;
-use App\Filament\Resources\StateResource\RelationManagers;
-use App\Models\State;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\State;
+use Filament\Infolists;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Infolists\Components\Section;
+//use Filament\Forms\Components\Section;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Infolists\Components\TextEntry;
+use App\Filament\Resources\StateResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\StateResource\RelationManagers;
+use Filament\Infolists\Components\Section as ComponentsSection;
+
+
 
 class StateResource extends Resource
 {
@@ -33,17 +44,17 @@ class StateResource extends Resource
         return $form
             ->schema([
                 Forms\Components\Section::make('State')
-                ->description('Put the state details in.')
-                ->schema([
-                Forms\Components\Select::make('country_id')
-                    ->relationship(name: 'country', titleAttribute: 'name')
-                    ->searchable()
-                    ->preload()
-                    ->required(),
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                ])->columns(2)
+                    ->description('Put the state details in.')
+                    ->schema([
+                        Forms\Components\Select::make('country_id')
+                            ->relationship(name: 'country', titleAttribute: 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                    ])->columns(2)
             ]);
     }
 
@@ -51,30 +62,51 @@ class StateResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('country_id')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('name')
+                Tables\Columns\TextColumn::make('country.name')
+                    ->label('Country Name')
+                    ->sortable()
                     ->searchable(),
+                //->searchable(isIndividual:true, isGlobal:false),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('State Name')
+                    ->sortable()
+                    ->searchable(),
+                //->hidden(!auth()->user()->email === 'rameez@mail.com'), // Used to hide the for specific user
+                //->visible(!auth()->user()->email === 'rameez@mail.com'), // Used to visible the for specific user
                 Tables\Columns\TextColumn::make('created_at')
+                    ->label('Created at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Updated at')
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
-            ])
+            ])->defaultSort('country.name')
             ->filters([
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
+            ]);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make('State Info')
+                    ->schema([
+                        TextEntry::make('country.name')->label('Country Name'),
+                        TextEntry::make('name')->label('State Name'),
+                    ])->columns(2),
             ]);
     }
 

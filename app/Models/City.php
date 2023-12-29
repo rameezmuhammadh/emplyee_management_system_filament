7909a0dc0eb27355395ab7fb11ed8674c3166cf3
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-29 11:36:20 +0530

    feat : hasMany relationships (Employee)

--- app/Models/City.php
+++ app/Models/City.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class City extends Model
 {
@@ -19,5 +20,9 @@ class City extends Model
     public function state():BelongsTo
     {
         return $this->belongsTo(State::class);
+    }
+
+    public function employees(): HasMany{
+        return $this->hasMany(Employee::class);
     }
 }

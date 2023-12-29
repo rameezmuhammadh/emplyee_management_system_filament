46a540d82821b8fdfc340067f3c417dccc7c1de0
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-29 11:36:44 +0530

    feat : hasMany relationships (Employee and state)

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Country extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function state():HasMany
+    {
+        return $this->hasMany(State::class);
+    }
+
+    public function employees(): HasMany{
+        return $this->hasMany(Employee::class);
+    }
 }

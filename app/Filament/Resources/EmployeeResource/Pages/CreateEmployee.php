148fa2cb5586b5b04038bfe9493c5c81be4120c2
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-29 22:13:52 +0530

    feat(createEmployee):  added the notifications

--- app/Filament/Resources/EmployeeResource/Pages/CreateEmployee.php
+++ app/Filament/Resources/EmployeeResource/Pages/CreateEmployee.php
@@ -4,9 +4,26 @@ namespace App\Filament\Resources\EmployeeResource\Pages;
 
 use App\Filament\Resources\EmployeeResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateEmployee extends CreateRecord
 {
     protected static string $resource = EmployeeResource::class;
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'New Employee created';
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Employee created.')
+            ->body('The Employee created successfully.');
+
+        // * return null did return any notification
+        // return null; 
+    }
 }

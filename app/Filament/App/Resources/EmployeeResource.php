7e604ef9e42482f584e820266251ada4a1190f94
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-30 15:44:15 +0530

    feat : created EmployeeResource for app Panel

---
+++ app/Filament/App/Resources/EmployeeResource.php
@@ -0,0 +1,259 @@
+<?php
+
+namespace App\Filament\App\Resources;
+
+use Filament\Forms;
+use App\Models\City;
+use Filament\Tables;
+use App\Models\State;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use App\Models\Employee;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Facades\Filament;
+use Illuminate\Support\Carbon;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
+use Illuminate\Support\Collection;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\App\Resources\EmployeeResource\Pages;
+use App\Filament\App\Resources\EmployeeResource\RelationManagers;
+
+class EmployeeResource extends Resource
+{
+    protected static ?string $model = Employee::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Forms\Components\Section::make('User Name')
+                ->description('Put the user name details in.')
+                ->schema([
+                    Forms\Components\TextInput::make('first_name')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('last_name')
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('middle_name')
+                        ->required()
+                        ->maxLength(255),
+                ])->columns(3),
+            Forms\Components\Section::make('User Address')
+                ->schema([
+                    Forms\Components\Textarea::make('address')
+                        ->autosize()
+                        ->required()
+                        ->maxLength(255),
+                    Forms\Components\TextInput::make('zip_code')
+                        ->required()
+                        ->maxLength(255),
+                ])->columns(2),
+            Forms\Components\Section::make('Dates')
+                ->schema([
+                    Forms\Components\DatePicker::make('date_hired')
+                        ->native(false)
+                        ->displayFormat('d/m/Y')
+                        ->required(),
+                    Forms\Components\DatePicker::make('date_of_birth')
+                        ->native(false)
+                        ->displayFormat('d/m/Y')
+                        ->required(),
+                ])->columns(2),
+            Forms\Components\Section::make('Employee Relationship')
+                ->schema([
+                    Forms\Components\Select::make('country_id')
+                        ->relationship(name: 'country',titleAttribute: 'name')
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->afterStateUpdated(function (Set $set) {
+                            $set('state_id', null);
+                            $set('city_id', null);
+                        })
+                        ->required(),
+                    Forms\Components\Select::make('state_id')
+                        ->options(fn (Get $get): Collection => State::query()
+                            ->where('country_id', $get('country_id'))
+                            ->pluck('name', 'id'))
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->afterStateUpdated(fn (Set $set) => $set('city_id', null))
+                        ->required(),
+                    Forms\Components\Select::make('city_id')
+                        ->options(fn (Get $get): Collection => City::query()
+                            ->where('state_id', $get('state_id'))
+                            ->pluck('name', 'id'))
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->required(),
+                    Forms\Components\Select::make('department_id')
+                        ->relationship(
+                            name: 'Department',
+                             titleAttribute: 'name',
+                             modifyQueryUsing: fn(Builder $query)=> $query->whereBelongsTo(Filament::getTenant())
+                             )
+                        ->searchable()
+                        ->preload()
+                        ->required(),
+                ])->columns(2)
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+            Tables\Columns\TextColumn::make('country.name')
+                ->sortable()
+                ->searchable()
+                ->toggleable(isToggledHiddenByDefault: false),
+            Tables\Columns\TextColumn::make('state.name')
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('city.name')
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('department.name')
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('first_name')
+                ->sortable()
+                ->searchable(),
+            Tables\Columns\TextColumn::make('last_name')
+                ->sortable()
+                ->searchable(),
+            Tables\Columns\TextColumn::make('middle_name')
+                ->searchable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('address')
+                ->searchable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('zip_code')
+                ->searchable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('date_of_birth')
+                ->date()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('date_hired')
+                ->date()
+                ->sortable(),
+            Tables\Columns\TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+        ])
+        ->filters([
+            SelectFilter::make('Department')
+                ->relationship('department', 'name')
+                ->searchable()
+                ->multiple()
+                ->preload()
+                ->label('Filter By Department')
+                ->indicator('Department'),
+
+            Filter::make('created_at')
+                ->form([
+                    DatePicker::make('created_from')->native(false),
+                    DatePicker::make('created_until')->native(false),
+                ])
+                ->query(function (Builder $query, array $data): Builder {
+                    return $query
+                        ->when(
+                            $data['created_from'],
+                            fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                        )
+                        ->when(
+                            $data['created_until'],
+                            fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                        );
+                })
+                ->indicateUsing(function (array $data): array {
+                    $indicators = [];
+
+                    if ($data['created_from'] ?? null) {
+                        $indicators['created_from'] = 'Created from ' . Carbon::parse($data['created_from'])->toFormattedDateString();
+                    }
+
+                    if ($data['created_until'] ?? null) {
+                        $indicators['created_until'] = 'Created until ' . Carbon::parse($data['created_until'])->toFormattedDateString();
+                    }
+
+                    return $indicators;
+                }),
+            //->columnSpan(2)->columns(2),
+        ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make('Employee Name')
+                    ->schema([
+                        TextEntry::make('first_name')->label('First Name'),
+                        TextEntry::make('middle_name')->label('Middle Name'),
+                        TextEntry::make('last_name')->label('Last Name'),
+                    ])->columns(3),
+
+                Section::make('Employee Address')
+                    ->schema([
+                        TextEntry::make('address')->label('Address'),
+                        TextEntry::make('zip_code')->label('Zip Code'),
+                    ])->columns(2),
+                Section::make('Employee Dates')
+                    ->schema([
+                        TextEntry::make('date_of_birth')->label('Date of Birth'),
+                        TextEntry::make('date_hired')->label('Date Hired'),
+                    ])->columns(2),
+                Section::make('Employee Relationships')
+                    ->schema([
+                        TextEntry::make('country.name')->label('Country Name'),
+                        TextEntry::make('state.name')->label('State Name'),
+                        TextEntry::make('city.name')->label('City Name'),
+                        TextEntry::make('department.name')->label('Department Name'),
+                    ])->columns(2)
+            ]);
+    }
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEmployees::route('/'),
+            'create' => Pages\CreateEmployee::route('/create'),
+            'view' => Pages\ViewEmployee::route('/{record}'),
+            'edit' => Pages\EditEmployee::route('/{record}/edit'),
+        ];
+    }
+}

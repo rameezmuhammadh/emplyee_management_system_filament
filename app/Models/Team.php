837b19c0900ba341b6fc425981188138d2afc31f
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-30 12:51:11 +0530

    feat: (Team model) create the team model and hasMany belongstoMany function

---
+++ app/Models/Team.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function employees():HasMany
+    {
+        return $this->hasMany(Employee::class);
+    }
+    public function departments():HasMany
+    {
+        return $this->hasMany(Department::class);
+    }
+    public  function members():BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}

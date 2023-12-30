09a3e960c5f15eedc1166e2190c9c371b4e94ec9
Rameez <121714501+rameezmuhammadh@users.noreply.github.com>
2023-12-30 15:39:33 +0530

    feat : added the tenant relationship functions

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -28,4 +28,9 @@ class Employee extends Model
     {
         return $this->belongsTo(Department::class);
     }
+
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
 }
